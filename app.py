7021bd59f418f8417be9ade15404cfd946187af0
Yas-hwanth <127596799+Yas-hwanth@users.noreply.github.com>
2026-04-17 19:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -437,3 +437,8 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
